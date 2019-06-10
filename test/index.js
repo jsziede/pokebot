ac695b2e70a2ab6ece36f52451c6a2f066eb15a2
jsziede <jsziede@yahoo.com>
2019-06-09 22:37:11 -0400

    Begin writing tests for item files.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,56 @@
-var index = require('../index.js');
-var assert = require('assert');
-var expect = require('chai').expect;
-var should = require('chai').should();
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
 
-/** Tests go here. */
+const fs = require('fs');
+
+const errors = 0;
+
+/**
+ * Determine integrity of file items.
+ */
+function testItemFiles() {
+    let itemDir = "./data/items";
+    fs.readdirSync(itemDir).forEach(file => {
+        let itemPath = itemDir + "/" + file;
+        let itemJSON = fs.readFileSync(itemPath);  
+        let item = JSON.parse(itemJSON);
+
+        /**
+         * Assert file name is consistent with the item name.
+         */
+        let itemName = item.name;
+        itemName = itemName.replace(/./gi, '');
+        itemName = itemName.replace(/-/gi, '_');
+        itemName = itemName.replace(/'/gi, '_');
+        itemName = itemName.replace(/ /gi, '_');
+        itemName = itemName.replace(/é/gi, 'e');
+        itemName = itemName + ".json";
+        assert.equal(file, itemName);
+
+        /**
+         * Check if item has all properties.
+         */
+        expect(item).to.have.property('name');
+        expect(item).to.have.property('sell_value');
+        expect(item).to.have.property('holdable');
+        expect(item).to.have.property('battle');
+        expect(item).to.have.property('category');
+        expect(item).to.have.property('subcategory');
+
+        /**
+         * Assert item properties are proper types.
+         */
+        assert.isString(item.name);
+        assert.isBoolean(item.holdable);
+        assert.isBoolean(item.battle);
+        expect(item.sell_value).to.satisfy(function(value) {
+            return (isNull(value) || (typeof value === "number"));
+        });
+
+    });
+}
+
+testItemFiles();
